558c8faf4d12e33bb35c88a902040f7aeb4336ad
Wycliffe <nwycliffe@sendy.co.ke>
2020-07-30 13:10:58 +0300

    runBlocking
    Synchronous co-routine scope.

--- MyCouroutines/app/src/main/java/com/example/wycliffenyakemwa/mycouroutines/MainActivity.kt
+++ MyCouroutines/app/src/main/java/com/example/wycliffenyakemwa/mycouroutines/MainActivity.kt
@@ -14,13 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "Oncreate Thread : ${Thread.currentThread().name}")
+     //   Log.d(TAG, "Oncreate Thread : ${Thread.currentThread().name}")
 
         //start a co-routine
         GlobalScope.launch {
 
             delay(4000L) // pauses current co-routine. Only run inside co-routine or another suspend function.
-            Log.d(TAG, "Thread launched by co-routine : ${Thread.currentThread().name}")
+          //  Log.d(TAG, "Thread launched by co-routine : ${Thread.currentThread().name}")
 
             //Suspend functions (sequential execution)
             val networkCall1 = doNetworkCall()
@@ -39,19 +39,46 @@ class MainActivity : AppCompatActivity() {
          * newSingleThreadContext(name) : Your own
          */
 
-        GlobalScope.launch(newSingleThreadContext("MyThread")) {  }
+        GlobalScope.launch(newSingleThreadContext("MyThread")) { }
 
         GlobalScope.launch(Dispatchers.IO) {
-            Log.d(TAG, "Thread launched by co-routine net call : ${Thread.currentThread().name}")
+         //   Log.d(TAG, "Thread launched by co-routine net call : ${Thread.currentThread().name}")
             val response = doNetworkCall()
 
             // switch context
-            withContext(Dispatchers.Main){
-                Log.d(TAG, "Thread launched for response: ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main) {
+              //  Log.d(TAG, "Thread launched for response: ${Thread.currentThread().name}")
                 tvTest.text = response
             }
         }
 
+
+        /**
+         * runBlocking : not asynchronous / in-sync with normal main thread flow
+         * stops the main thread
+         * usecase1 : if you need to call suspend functions
+         * usecase2 : junit testing, to use suspend functions from inside  a test function
+         */
+        runBlocking {
+
+            //can also run an async coroutine scope
+            launch(Dispatchers.IO) {
+                delay(3000L)
+                Log.d(TAG, "After IO 1")
+            }
+
+            launch(Dispatchers.IO) {
+                delay(3000L)
+                Log.d(TAG, "After IO 2")
+            }
+
+            Log.d(TAG, "Before Delay")
+            delay(5000L)
+            Log.d(TAG, "After Delay")
+
+
+
+        }
     }
 
     private suspend fun doNetworkCall(): String {
